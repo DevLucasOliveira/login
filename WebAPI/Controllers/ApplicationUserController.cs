5ff8d6b2f5d721e20f8901b93625bf022e2a4a15
Lucas Oliveira <lucasoliveira.si@outlook.com>
2020-03-25 15:09:59 -0300

    injecting JWT on aspnet webapi

--- WebAPI/Controllers/ApplicationUserController.cs
+++ WebAPI/Controllers/ApplicationUserController.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using WebAPI.Models;
 
 namespace WebAPI.Controllers
@@ -16,11 +21,17 @@ namespace WebAPI.Controllers
 
         private UserManager<ApplicationUser> _userManager;
         private SignInManager<ApplicationUser> _signInManager;
+        private SecurityTokenDescriptor tokenDescriptor;
+        private SecurityToken securityToken;
+        private readonly ApplicationSettings _appSettings;
 
-        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        public SigningCredentials SigningCredentials { get; private set; }
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IOptions<ApplicationSettings> appSettings)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpPost]
@@ -41,14 +52,48 @@ namespace WebAPI.Controllers
                 var result = await _userManager.CreateAsync(applicationUser, model.Password);
                 return Ok(result);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw ex;
             }
+        }
 
+        [HttpPost]
+        [Route("Login")]
+        //POST : /api/ApplicationUser/Login
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim("UserID", user.Id.ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddMinutes(5),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSettings.JWT_Secret)), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var SecurityToken = tokenHandler.CreateToken(tokenDescriptor);
+                var token = tokenHandler.WriteToken(securityToken);
+                return OkResult(new { token });
+            }
 
+            else
+                return BadRequestResult(new { message = "Usuário ou senha incorreta." });
+            
+        }
 
+        private IActionResult OkResult(object p)
+        {
+            throw new NotImplementedException();
+        }
 
+        private IActionResult BadRequestResult(object p)
+        {
+            throw new NotImplementedException();
         }
     }
-}
+  }
